f86f56eb7fa8ea477f39ad8bcacfecd541618f46
hyunh84 <hyunh84@hanmail.net>
2021-07-23 14:44:34 +0900

    2021-07-23:3

--- resources/js/ui_style.js
+++ resources/js/ui_style.js
@@ -1,5 +1,5 @@
 // Input Text, Search active
-$(document).on('focusin change', '[class^="inpTxt"] input[type="text"], [class^="inpTxt"] input[type="tel"], [class^="inpTxt"] input[type="password"]', function() {
+$(document).on('click focusin change', '[class^="inpTxt"] input[type="text"], [class^="inpTxt"] input[type="tel"], [class^="inpTxt"] input[type="password"]', function() {
 	var _this = $(this);
 	var _val = _this.val();
 	var _inpBox = _this.closest('[class^="inpTxt"]');
